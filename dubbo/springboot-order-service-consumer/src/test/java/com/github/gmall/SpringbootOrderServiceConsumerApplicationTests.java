c306c491441792928b5ad5a945cb66b80dd98d89
andochiwa <a1066079469@gmail.com>
2021-03-14 03:21:17 +0900

    spring-dubbo

---
+++ dubbo/springboot-order-service-consumer/src/test/java/com/github/gmall/SpringbootOrderServiceConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOrderServiceConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ dubbo/springboot-user-service-provider/src/main/java/com/github/gmall/SpringbootUserServiceProviderApplication.java
@@ -0,0 +1,13 @@
+package com.github.gmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUserServiceProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootUserServiceProviderApplication.class, args);
+    }
+
+}
